a7b01ac929b04f2e85db16b560d190e97483aebe
Vikrum Nijjar <vsnijjar@gmail.com>
2021-03-01 12:32:52 -0800

    Updated report template.

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ mkdir -p $DIR/dist
 
 cd $DIR
 
-GOOS=darwin GOARCH=amd64 go build -o dist/rpCheckup_darwin_x86
+GOOS=darwin GOARCH=amd64 go build -o dist/rpCheckup_darwin_amd64
 GOOS=darwin GOARCH=arm64 go build -o dist/rpCheckup_darwin_arm64
 
 GOOS=linux GOARCH=amd64 go build -o dist/rpCheckup_linux
